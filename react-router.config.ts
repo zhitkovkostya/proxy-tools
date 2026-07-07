19d9a8d87c0caefb8d0744879ae9ee1549fe82dd
Konstantin Zhitkov <kzhitkov@adsterra.com>
2026-07-07 18:10:57 +0300

    fix(ci): fix routing error

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,4 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
   prerender: true,
-  basename: process.env.GITHUB_ACTIONS === "true" ? "/proxy-tools" : "/",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => ({
-  base: process.env.GITHUB_ACTIONS === "true" ? "/proxy-tools/" : "/",
+  base: "./",
   plugins: [tailwindcss(), reactRouter()],
   resolve: {
     tsconfigPaths: true,
